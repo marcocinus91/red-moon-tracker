efbd036077fa8258ecd5c5ecaad3efa56f803ba5
Marco <marco.cinus@abinsula.com>
2026-02-19 15:52:11 +0100

    update: install tres js

--- src/main.js
+++ src/main.js
@@ -4,8 +4,9 @@ import App from "./App.vue";
 import router from "./router";
 import "./style.css";
 import piniaPersistedstate from "pinia-plugin-persistedstate";
+import Tres from '@tresjs/core';
 
 const pinia = createPinia();
 pinia.use(piniaPersistedstate);
 
-createApp(App).use(pinia).use(router).mount("#app");
+createApp(App).use(Tres).use(pinia).use(router).mount("#app");
